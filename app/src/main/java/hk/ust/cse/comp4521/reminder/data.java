25b7f9f0b8f46af877eab2a0001f0a3002ecafc2
kcleungam <kcleungam@ust.hk>
2016-05-09 22:57:54 +0800

    .

---
+++ app/src/main/java/hk/ust/cse/comp4521/reminder/data.java
@@ -0,0 +1,42 @@
+package hk.ust.cse.comp4521.reminder;
+
+import java.sql.Time;
+
+/**
+ * Created by Krauser on 9/5/2016.
+ */
+public class data {
+    public static enum ReminderType{Time, Location}
+
+    public ReminderType reminderType = null;
+    public String title = null;
+    public Time time = null;
+    public Time validUntil = null;                     // location event may valid until a certain time
+    public boolean[] repeat = new boolean[7];       // Store the day for repetition, Mon to Fri
+    public String description = null;
+    // Location
+    // Image
+
+    /**
+     *         Location event constructor, should inculde:      location, title, validUntil, description, image
+     */
+    public data(String title, Time validUntil, String description){  // need to add location and image
+        this.reminderType = ReminderType.Location;
+        this.title = title;
+        this.validUntil = validUntil;
+        this.description = description;
+    }
+
+    /**
+     *          TIme event constructor, should include:     time, title, repeat, description,
+     */
+    public data(String title, Time time, boolean[] repeat, String description){     // need to add location and image
+        this.reminderType = ReminderType.Time;
+        this.title = title;
+        this.time = time;
+        this.repeat = repeat;
+        this.description = description;
+    }
+
+}
+
