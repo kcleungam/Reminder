a9ece4124d1b423d19e036220581f48326630be5
kcleungam <kcleungam@ust.hk>
2016-05-11 22:36:53 +0800

    .

--- app/src/main/java/hk/ust/cse/comp4521/reminder/MainActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/reminder/MainActivity.java
@@ -8,24 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView reminderList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        reminderList = (ListView)findViewById(R.id.reminder_list);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
