f56c1b7287e9a595c1dcf92746f0b73c3236da9d
kcleungam <kcleungam@ust.hk>
2016-05-13 23:42:46 +0800

    .

--- app/src/main/java/hk/ust/cse/comp4521/reminder/ViewTimeActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/reminder/ViewTimeActivity.java
@@ -1,26 +1,21 @@
 package hk.ust.cse.comp4521.reminder;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 public class ViewTimeActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.view_time_container);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_time_container);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+            String title = getIntent().getStringExtra("title");
 
-        String title = getIntent().getStringExtra("title");
-
-        TextView showTitle = (TextView) findViewById(R.id.showTitle);
-        showTitle.setText(title);
-    }
+            TextView showTitle = (TextView) findViewById(R.id.showTitle);
+            showTitle.setText(title);
+        }
 
 }
+
