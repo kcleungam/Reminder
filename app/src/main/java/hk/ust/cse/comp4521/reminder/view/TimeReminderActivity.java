01e77d91ed5d701a5d8e570de339a1233bca33db
Fire-Prometheus <alextowunyin@gmail.com>
2016-05-22 18:33:24 +0800

    Changed the UI for time reminders
    Some bugs still needs to be fixed

--- app/src/main/java/hk/ust/cse/comp4521/reminder/view/TimeReminderActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/reminder/view/TimeReminderActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -19,14 +20,18 @@ import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.IllegalFormatCodePointException;
 
 import hk.ust.cse.comp4521.reminder.R;
 import hk.ust.cse.comp4521.reminder.data.DataController;
@@ -34,11 +39,11 @@ import hk.ust.cse.comp4521.reminder.data.ReminderData;
 import hk.ust.cse.comp4521.reminder.util.DateTimeParser;
 
 public class TimeReminderActivity extends AppCompatActivity {
-
+    /* variables */
+    //data controls
     private DataController dataController;
-
     private ReminderData reminderData;
-    private TextView editTitle;
+    /*private TextView editTitle;
     private TextView editTime;
     private ImageButton timeButton;
     private CheckBox[] checkBoxes;
@@ -46,8 +51,15 @@ public class TimeReminderActivity extends AppCompatActivity {
     private TextView locationText;
     private ImageButton locationButton;
     private TextView editDescription;
-    private ImageView imageView;
+    private ImageView imageView;*/
 
+    //UI component controls
+    private MaterialEditText title,time,location,description;
+    private Switch repeat;
+    private CheckBox[] checkBoxes;
+    private ImageView image;
+
+    //UI related controls
     private static final int PICK_IMAGE = 1;
     public static final int RETURN_LOCATION = 2;
 
@@ -58,34 +70,72 @@ public class TimeReminderActivity extends AppCompatActivity {
     //@TargetApi(23)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         dataController = DataController.getInstance(getApplication());
 
+        /* map UI components */
         setContentView(R.layout.edit_time_container);
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.editTimeLayout);
-        editTitle = (TextView) layout.findViewById(R.id.editTitle);
-        editTime = (TextView) layout.findViewById(R.id.editTime);
-        editTime.setOnClickListener(new View.OnClickListener() {
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.setTimeReminderLayout);
+
+        title = (MaterialEditText) layout.findViewById(R.id.title);
+        time = (MaterialEditText) layout.findViewById(R.id.time);
+        location = (MaterialEditText) layout.findViewById(R.id.location);
+        description = (MaterialEditText) layout.findViewById(R.id.description);
+        repeat=(Switch)layout.findViewById(R.id.repeat_switch);
+        checkBoxes = new CheckBox[7];
+        checkBoxes[1] = (CheckBox) layout.findViewById(R.id.repeat_mon);
+        checkBoxes[2] = (CheckBox) layout.findViewById(R.id.repeat_tue);
+        checkBoxes[3] = (CheckBox) layout.findViewById(R.id.repeat_wed);
+        checkBoxes[4] = (CheckBox) layout.findViewById(R.id.repeat_thu);
+        checkBoxes[5] = (CheckBox) layout.findViewById(R.id.repeat_fri);
+        checkBoxes[6] = (CheckBox) layout.findViewById(R.id.repeat_sat);
+        checkBoxes[0] = (CheckBox) layout.findViewById(R.id.repeat_sun);
+        image=(ImageView)layout.findViewById(R.id.image);
+
+        /* set up listeners */
+        time.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialog(R.id.editTime);
+                showDialog(R.id.time);
             }
         });
-        timeButton = (ImageButton) layout.findViewById(R.id.timeButton);
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivityForResult(intent, RETURN_LOCATION);
+            }
+        });
+        repeat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //enable/disable all checkboxes
+                for(CheckBox checkBox:checkBoxes)
+                    checkBox.setEnabled(isChecked);
+            }
+        });
+        //TODO: listeners for checkboxes
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
+                startActivityForResult(chooserIntent, PICK_IMAGE);
+            }
+        });
+        /*timeButton = (ImageButton) layout.findViewById(R.id.timeButton);
         timeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDialog(R.id.timeButton);
             }
-        });
-        checkBoxes = new CheckBox[7];
-        checkBoxes[1] = (CheckBox) layout.findViewById(R.id.monBox);
-        checkBoxes[2] = (CheckBox) layout.findViewById(R.id.tueBox);
-        checkBoxes[3] = (CheckBox) layout.findViewById(R.id.wedBox);
-        checkBoxes[4] = (CheckBox) layout.findViewById(R.id.thurBox);
-        checkBoxes[5] = (CheckBox) layout.findViewById(R.id.friBox);
-        checkBoxes[6] = (CheckBox) layout.findViewById(R.id.satBox);
-        checkBoxes[0] = (CheckBox) layout.findViewById(R.id.sunBox);
-        CompoundButton.OnClickListener wkdayBoxListener = new CompoundButton.OnClickListener() {
+        });*/
+
+        /*CompoundButton.OnClickListener wkdayBoxListener = new CompoundButton.OnClickListener() {
             @Override
             public void onClick(View v) {
                 for (CheckBox box : checkBoxes) {
@@ -99,8 +149,8 @@ public class TimeReminderActivity extends AppCompatActivity {
         };
         for(CheckBox checkBox: checkBoxes){
             checkBox.setOnClickListener(wkdayBoxListener);
-        }
-        selectAllButton = (Button) layout.findViewById(R.id.selectAll);
+        }*/
+        /*selectAllButton = (Button) layout.findViewById(R.id.selectAll);
         View.OnClickListener selectAllListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) { //if all box checked, set button to "unselect all", vice versa
@@ -111,8 +161,8 @@ public class TimeReminderActivity extends AppCompatActivity {
             }
         };
         selectAllButton.setOnClickListener(selectAllListener);
-        selectAllButton.setTag(true);
-        locationText = (TextView) layout.findViewById(R.id.editLocation);
+        selectAllButton.setTag(true);*/
+        /*locationText = (TextView) layout.findViewById(R.id.editLocation);
         locationButton = (ImageButton) layout.findViewById(R.id.locationButton);
         locationButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -120,43 +170,44 @@ public class TimeReminderActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
                 startActivityForResult(intent, RETURN_LOCATION);
             }
-        });
-        editDescription = (TextView) layout.findViewById(R.id.editDescription);
-        imageView = (ImageView) layout.findViewById(R.id.imageView);
+        });*/
+        /*editDescription = (TextView) layout.findViewById(R.id.editDescription);*/
+        /*imageView = (ImageView) layout.findViewById(R.id.imageView);
         ImageView.OnClickListener imageViewListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 getIntent.setType("image/*");
-                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 pickIntent.setType("image/*");
                 Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
                 chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
                 startActivityForResult(chooserIntent, PICK_IMAGE);
             }
         };
-        imageView.setOnClickListener(imageViewListener);
+        imageView.setOnClickListener(imageViewListener);*/
 
+        /* initialise the UI components */
         long reminderId = getIntent().getLongExtra("ReminderId", -1);
         if (reminderId != -1)
             reminderData = dataController.getReminder(reminderId);
         else
             reminderData = new ReminderData();
         if (reminderData.getId()!=-1) {
-            editTitle.setText(reminderData.getTitle());
-            editTime.setText(reminderData.getTime());
+            title.setText(reminderData.getTitle());
+            time.setText(reminderData.getTime());
             for (int i = 0; i < checkBoxes.length; i++) {
                 checkBoxes[i].setChecked(reminderData.getRepeat()[i]);
             }
-            setSelectAll(!Arrays.asList(reminderData.getRepeat()).contains(false));
-            editDescription.setText(reminderData.getDescription());
-            locationText.setText(reminderData.getLocation());
+            //setSelectAll(!Arrays.asList(reminderData.getRepeat()).contains(false));
+            description.setText(reminderData.getDescription());
+            location.setText(reminderData.getLocation());
             if(reminderData.getImageUri()!=null) {
                 try {
                     Uri imageUri = Uri.parse(reminderData.getImageUri());
                     InputStream imageStream = getContentResolver().openInputStream(imageUri);
                     Bitmap image = BitmapFactory.decodeStream(imageStream);
-                    imageView.setImageBitmap(image);
+                    this.image.setImageBitmap(image);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
@@ -164,6 +215,7 @@ public class TimeReminderActivity extends AppCompatActivity {
         }
     }
 
+    /*@Deprecated
     private void setSelectAll(boolean selectAll){
         if(selectAll){
             selectAllButton.setText("Select all");
@@ -172,7 +224,7 @@ public class TimeReminderActivity extends AppCompatActivity {
             selectAllButton.setText("Unselect all");
             selectAllButton.setTag(false);
         }
-    }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -186,24 +238,24 @@ public class TimeReminderActivity extends AppCompatActivity {
 
         switch (item_id) {
             case R.id.action_save:
-                if(editTitle.getText().length()==0){
+                if(title.getText().length()==0){
                     Toast.makeText(TimeReminderActivity.this, "Empty title", Toast.LENGTH_SHORT).show();
                     return true;
                 }
-                if(!DateTimeParser.validate(editTime.getText().toString(), DateTimeParser.Format.SHORT)){
+                if(!DateTimeParser.validate(time.getText().toString(), DateTimeParser.Format.SHORT)){
                     Toast.makeText(TimeReminderActivity.this, "Empty time", Toast.LENGTH_SHORT).show();
                     return true;
                 }
                 reminderData.setReminderType(ReminderData.ReminderType.Time);
-                reminderData.setTitle(editTitle.getText().toString());
-                reminderData.setTime(editTime.getText().toString());
+                reminderData.setTitle(title.getText().toString());
+                reminderData.setTime(time.getText().toString());
                 boolean[] repeat = new boolean[checkBoxes.length];
                 for (int i = 0; i < checkBoxes.length; i++) {
                     repeat[i] = checkBoxes[i].isChecked();
                 }
                 reminderData.setRepeat(repeat);
-                reminderData.setLocation(locationText.getText().toString());
-                reminderData.setDescription(editDescription.getText().toString());
+                reminderData.setLocation(location.getText().toString());
+                reminderData.setDescription(description.getText().toString());
                 if (reminderData.getId() < 0) {
                     reminderData.setEnabled(true);
                     dataController.addReminder(reminderData);
@@ -231,7 +283,7 @@ public class TimeReminderActivity extends AppCompatActivity {
                 Uri imageUri = data.getData();
                 InputStream imageStream = getContentResolver().openInputStream(imageUri);
                 Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                imageView.setImageBitmap(selectedImage);
+                this.image.setImageBitmap(selectedImage);
                 reminderData.setImageUri(data.getData().toString());
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -246,7 +298,7 @@ public class TimeReminderActivity extends AppCompatActivity {
                 String locationName = data.getStringExtra("locationName");
                 double latitude = data.getDoubleExtra("latitude", -999);
                 double longitude = data.getDoubleExtra("longitude", -999);
-                locationText.setText(locationName);
+                location.setText(locationName);
                 reminderData.setLatitude(latitude);
                 reminderData.setLongitude(longitude);
             } catch (Exception f){
@@ -270,8 +322,8 @@ public class TimeReminderActivity extends AppCompatActivity {
     @Override
     protected Dialog onCreateDialog(int id) {
         switch (id) {
-            case R.id.timeButton:
-            case R.id.editTime:
+            //case R.id.timeButton:
+            case R.id.time:
                 // set time picker as current time
                 TimePickerDialog timePickerDialog;
                 if(reminderData.getId()!=-1) {
@@ -292,7 +344,7 @@ public class TimeReminderActivity extends AppCompatActivity {
             Calendar now = Calendar.getInstance();
             now.set(Calendar.HOUR_OF_DAY, hourOfDay);
             now.set(Calendar.MINUTE, min);
-            editTime.setText(DateTimeParser.toString(now.getTimeInMillis(), DateTimeParser.Format.SHORT));
+            time.setText(DateTimeParser.toString(now.getTimeInMillis(), DateTimeParser.Format.SHORT));
         }
     };
 
