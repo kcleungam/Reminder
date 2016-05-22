24900fe77564e2a0d065361e229f57040c73b05c
kcleungam <kcleungam@ust.hk>
2016-05-22 20:50:46 +0800

    add name, id, email at top

--- app/src/main/java/hk/ust/cse/comp4521/reminder/view/MainActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/reminder/view/MainActivity.java
@@ -1,3 +1,8 @@
+//      Comp 4521
+//      Leung Ka Chun       20125844        kcleungam@ust.hk
+//      To Wun Yin            20112524        wytoaa@ust.hk
+//      Leung Chun Fai      20113619        cfleungac@ust.hk
+
 package hk.ust.cse.comp4521.reminder.view;
 
 import android.Manifest;
